ddcc4c1d8e673311a6f1dc19d4585b8a19c86fc9
Ricardo Duarte <ricardohtduarte@gmail.com>
2022-01-30 20:27:04 +0000

    New file organization

--- Sources/NeoNetwork/URLSessionProtocol.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-protocol URLSessionProtocol {
-    @available(macOS 10.15, *)
-    func dataTaskPublisher(for url: URL) -> URLSession.DataTaskPublisher
-}
-
-extension URLSession: URLSessionProtocol {}
