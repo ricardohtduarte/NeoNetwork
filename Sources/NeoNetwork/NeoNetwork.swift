d5dd82d2cbe545d792f9d9995f077dbd064fda30
Ricardo Duarte <ricardohtduarte@gmail.com>
2022-02-02 21:59:13 +0000

    Add completion handler and async await version

--- Sources/NeoNetwork/NeoNetwork.swift
+++ Sources/NeoNetwork/NeoNetwork.swift
@@ -28,4 +28,31 @@ extension NeoNetwork: NeoNetworkProtocol {
             .receive(on: fetcherQueue)
             .eraseToAnyPublisher()
     }
+
+    public func fetch<T: Decodable>(from url: URL, completionHandler: @escaping (Result<T?, Error>) -> Void) {
+        urlSession.dataTask(with: url, completionHandler: { data, urlResponse, error in
+            if let error = error {
+                completionHandler(.failure(error))
+            }
+
+            guard let data = data else {
+                completionHandler(.success(nil))
+                return
+            }
+
+            do {
+                let decodedData = try JSONDecoder().decode(T.self, from: data)
+                completionHandler(.success(decodedData))
+            } catch let error {
+                completionHandler(.failure(error))
+            }
+        }).resume()
+    }
+
+    @available(iOS 15.0, *)
+    public func fetch<T: Decodable>(from url: URL) async throws -> T {
+        let (data, _) = try await urlSession.data(from: url, delegate: nil)
+        let decodedData = try decoder.decode(T.self, from: data)
+        return decodedData
+    }
 }
--- Sources/NeoNetwork/URLSessionProtocol.swift
+++ Sources/NeoNetwork/URLSessionProtocol.swift
@@ -1,8 +1,11 @@
 import Foundation
 
 public protocol URLSessionProtocol {
-    func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask
+    func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask
     func dataTaskPublisher(for url: URL) -> URLSession.DataTaskPublisher
+
+    @available(iOS 15.0, *)
+    func data(from url: URL, delegate: URLSessionTaskDelegate?) async throws -> (Data, URLResponse)
 }
 
 extension URLSession: URLSessionProtocol {}
